f97c4ff6a6594ae4b275494cff4d7685e8d4a2d1
jezinka <jezina1989@gmail.com>
2023-04-17 17:39:14 +0200

    filter collection

--- ui/src/MySet.tsx
+++ ui/src/MySet.tsx
@@ -4,20 +4,30 @@ import axios from "axios";
 
 const MySet = () => {
     const [cards, setCards] = useState([]);
+    const [name, setName] = useState("");
 
     useEffect(() => {
-        fetchCards();
+        fetchCards(null);
     }, []);
 
-    const fetchCards = async () => {
-        const response = await axios.get("http://localhost:3000/mySet/");
+    const fetchCards = async (event:any) => {
+        if(event != null) {
+            event.preventDefault();
+        }
+        const response = await axios.get("http://localhost:3000/mySet/" + name);
         setCards(response.data);
     }
 
     return (
         <div>
             <h1>Kolekcja</h1>
-
+            <form onSubmit={fetchCards}>
+                <label>
+                    Nazwa karty:
+                    <input type="text" value={name} onChange={(event) => setName(event.target.value)}/>
+                </label>
+                <button type="submit">Submit</button>
+            </form>
             <div className="gallery">
                 {cards.map((card, index) => (
                     <img
