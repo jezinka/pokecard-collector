cef6f9b74b7dd136cb03b99549844ae0794b7f84
jezinka <jezina1989@gmail.com>
2023-05-01 07:36:03 +0200

    change port

--- ui/src/requests.ts
+++ ui/src/requests.ts
@@ -1,25 +1,25 @@
 import axios, {AxiosResponse} from 'axios';
 
 export async function savePokemon(card: any): Promise<AxiosResponse> {
-    return await axios.post("http://localhost:3000/addPokemon/", card);
+    return await axios.post("http://localhost:3005/addPokemon/", card);
 }
 
 export async function deletePokemon(id: any): Promise<AxiosResponse> {
-    return await axios.delete("http://localhost:3000/deletePokemon/" + id);
+    return await axios.delete("http://localhost:3005/deletePokemon/" + id);
 }
 
 export async function getCard(filterName: string): Promise<AxiosResponse> {
-    return await axios.get("http://localhost:3000/mySet/" + filterName);
+    return await axios.get("http://localhost:3005/mySet/" + filterName);
 }
 
 export async function getByName(name: string): Promise<AxiosResponse> {
-    return await axios.get("http://localhost:3000/pokemonCards/" + name);
+    return await axios.get("http://localhost:3005/pokemonCards/" + name);
 }
 
 export async function getDifferenceByCardset(cardset: string): Promise<AxiosResponse> {
-    return await axios.get("http://localhost:3000/all/" + cardset);
+    return await axios.get("http://localhost:3005/all/" + cardset);
 }
 
 export async function fetchAllCardsets(): Promise<AxiosResponse> {
-    return await axios.get("http://localhost:3000/cardset/");
+    return await axios.get("http://localhost:3005/cardset/");
 }
